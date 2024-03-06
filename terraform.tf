0efefec268a69ab7682f488862cb4df37756d5ac
kasamajay <kasamajay@gmail.com>
2024-03-06 19:15:12 +0000

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.6"  # Specifies the minimum Terraform version required
   backend "s3" {
     # Configuration for Terraform backend (e.g., for storing state)
-    bucket         = "aws-infra-state"
+    bucket         = "aws-infra-state-2023"
     key            = "terraform.tfstate"
     region         = "eu-west-2"
     dynamodb_table = "terraform-lock"
